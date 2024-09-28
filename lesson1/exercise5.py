f46352574dbb7a4304ead46d4a892621f7c40dc1
leopoldocosta <leopoldocosta@gmail.com>
2024-09-28 16:24:41 -0300

    String concatenation different methods; exercise6.

--- lesson1/exercise5.py
+++ lesson1/exercise5.py
@@ -10,7 +10,17 @@ def main():
     ip_addr = "10.12.17.1"
     mac_addr = "0024.c4e9.48ae"
 
-    print("\n" + ip_addr + " --> " + mac_addr + "\n")
+    print("\n1. String concatenation:")
+    print(ip_addr + " --> " + mac_addr + "\n")
+
+    # using f-strings
+    print("2. Using f-strings")
+    print(f"{ip_addr} --> {mac_addr}\n")
+
+    #using format() method
+    print("3. Using format method:")
+    print("{} --> {}\n".format(ip_addr,mac_addr))
+
 
 if __name__ == "__main__":
     # This block only runs if the script is executed directly
